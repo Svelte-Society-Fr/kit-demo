9b821c1cc7a28d5be9b9e6030581d1805ca9e967
Romain Crestey <rcrestey@gmail.com>
2023-10-20 14:22:25 +0200

    simplify fetchPokemon

--- src/lib/server/data.ts
+++ src/lib/server/data.ts
@@ -1,25 +1,7 @@
 import pokemons from '../../../data/pokemons.json';
 
-const url = 'https://pokeapi.co/api/v2/pokemon';
-
-function normalize(data: any) {
-  const copy = structuredClone(data);
-
-  delete copy.moves;
-  delete copy.abilities;
-  delete copy.game_indices;
-
-  return copy;
-}
-
 export async function fetchPokemon(id: number) {
-  // await new Promise(resolve => {
-  //   setTimeout(resolve, 2000);
-  // });
-
-  return fetch(`${url}/${id}`)
-    .then(r => r.json())
-    .then(normalize);
+  return Promise.resolve(pokemons[id - 1]);
 }
 
 export function fetchPokemons() {
