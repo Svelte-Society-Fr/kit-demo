8b27a1455d3fc4cbba462609effeddae16de13dd
Romain Crestey <rcrestey@gmail.com>
2023-11-09 13:17:32 +0100

    clear

--- src/lib/helpers.ts
+++
@@ -1,3 +0,0 @@
-export function getRandomNb(max: number, min = 0) {
-  return Math.floor(min + Math.random() * (max - min));
-}
--- src/routes/pokedex/[id]/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { fetchPokemon } from "$lib/server/data";
-
-export const load = async ({ params: { id } }) => {
-  return {
-    pokemon: fetchPokemon(parseInt(id)),
-  };
-};
